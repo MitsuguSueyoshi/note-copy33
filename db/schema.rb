75d30728f16c8e470690aed149973afd2e19a2cf
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-11-23 17:28:34 +0900

    Create notes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181118072734) do
+ActiveRecord::Schema.define(version: 20181123082614) do
+
+  create_table "notes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.integer "category", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "nickname", null: false
@@ -28,4 +38,5 @@ ActiveRecord::Schema.define(version: 20181118072734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notes", "users"
 end
