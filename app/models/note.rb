41bbe36e4e370949321809941fe7faebfdd9fd7a
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-11-23 21:17:31 +0900

    Set validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :category, presence: true
 
   enum category: { マンガ: 0, コラム: 1, 小説: 2, サウンド: 3, ビジネス: 4, ライフスタイル: 5, テクノロジー: 6, エンタメ: 7 }
 end
