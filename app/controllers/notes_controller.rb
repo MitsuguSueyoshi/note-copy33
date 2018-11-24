338f87575da8afccc7f053cce5126ad3421b7fff
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-11-24 19:00:37 +0900

    Add create method for authenticate_user!

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,5 @@
 class NotesController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @notes = Note.all.includes(:user).order("created_at DESC")
