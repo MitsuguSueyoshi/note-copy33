eaf643e9a549682b8975558949f7ff8f1f31cb17
末吉貢 <mitsugu11281128@gmail.com>
2018-11-24 19:23:10 +0900

    create process of following

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,16 @@
 class RelationshipsController < ApplicationController
-  before_action :require_user_logged_in
-
   def create
-    user = User.find(params[:follow_id])
+    user = User.find(params[:relationship][:follow_id])
     current_user.follow(user)
     flash[:success] = 'ユーザーをフォローしました'
     redirect_to user
   end
 
   def destroy
-    user = User.find(params[:follow_id])
+    user = User.find(params[:relationship][:follow_id])
     current_user.unfollow(user)
     flash[:success] = 'ユーザーのフォローを解除しました'
     redirect_to user
   end
+
 end
