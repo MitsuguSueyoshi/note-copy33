3593733af1259d24692e0bbbabc5bf695626a04f
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-11-24 17:57:23 +0900

    Delete unnecessary code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
  devise_for :users
  root 'notes#index'
- resources :notes, only: [:index, :show, :new, :create]
+ resources :notes, only: [:show, :new, :create]
 end
