26dffa97766f3ad2289c62cf631e7e0296b0e5f1
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-11-24 18:52:35 +0900

    Add space

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+
   def index
     @notes = Note.all.includes(:user).order("created_at DESC")
   end
