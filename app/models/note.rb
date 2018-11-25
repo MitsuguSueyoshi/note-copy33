01ca4f011a04d60f8d8ed4da8f795c9a6a7a05e1
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-11-25 14:55:48 +0900

    Add dependent: :destroy

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ApplicationRecord
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
 
   validates :title, presence: true
