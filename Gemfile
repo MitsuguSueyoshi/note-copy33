e088dce612c858774d79fe3061419cf11e813e30
末吉貢 <mitsugu11281128@gmail.com>
2018-11-25 13:41:27 +0900

    refactoring

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pry-rails'
 end
 
 group :production do
@@ -67,6 +68,5 @@ gem 'font-awesome-rails'
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
 gem 'devise'
-gem 'pry-rails'
 gem 'carrierwave'
 gem 'mini_magick'
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,16 +1,22 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user
+
   def create
-    user = User.find(params[:relationship][:follow_id])
     current_user.follow(user)
     flash[:success] = 'ユーザーをフォローしました'
     redirect_to user
   end
 
   def destroy
-    user = User.find(params[:relationship][:follow_id])
     current_user.unfollow(user)
     flash[:success] = 'ユーザーのフォローを解除しました'
     redirect_to user
+  end
+
+  private
+
+  def set_user
+    user = User.find(params[:relationship][:follow_id])
   end
 
 end
