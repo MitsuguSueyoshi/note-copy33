9e62917992737c0a3444029b6df68f8b5778ff62
末吉貢 <mitsugu11281128@gmail.com>
2018-11-25 15:46:39 +0900

    edit mig-file magazines

--- db/migrate/20181125063139_create_magazines.rb
+++ db/migrate/20181125063139_create_magazines.rb
@@ -1,7 +1,10 @@
 class CreateMagazines < ActiveRecord::Migration[5.1]
   def change
     create_table :magazines do |t|
-
+      t.string :title, null: false
+      t.string :header_image
+      t.text :description
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
