b3d2b79747fe6a1dabfababdbff9f72561e9eca2
miyaiaya <westtaila42240@gmail.com>
2018-12-01 19:00:05 +0900

    add preview function main_image(icon)

--- app/assets/javascripts/user-show.js
+++ app/assets/javascripts/user-show.js
@@ -25,4 +25,20 @@ $(document).on('turbolinks:load', function(){
     $(".header_present_image").remove();
     readURL(this);
   });
+
+  function readURL(input){
+    if(input.files && input.files[0]){
+      var reader = new FileReader();
+
+      reader.onload = function(e){
+        $("#main_image_prev").attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#user_main_image").change(function(){
+    $("#main_image_prev").removeClass("hidden").show();
+    $(".my-icon").remove();
+    readURL(this);
+  })
 });
