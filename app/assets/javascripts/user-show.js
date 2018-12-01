cbbc2fe9690c72c8b8ec19564f195c0358454639
miyaiaya <westtaila42240@gmail.com>
2018-12-01 17:50:47 +0900

    add preview function header_image

--- app/assets/javascripts/user-show.js
+++ app/assets/javascripts/user-show.js
@@ -9,4 +9,20 @@ $(document).on('turbolinks:load', function(){
     $(".user-container").hide();
     $(".user-profile").show();
   });
+
+  function readURL(input){
+    if(input.files && input.files[0]){
+      var reader = new FileReader();
+
+      reader.onload = function(e){
+        $("#header_image_prev").attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#user_header_image").change(function(){
+    $("#header_image_prev").removeClass("hidden").show();
+    $(".header_present_image").remove();
+    readURL(this);
+  });
 });
