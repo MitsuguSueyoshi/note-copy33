29c7de1a1b0b0ebad77dd08f0da15a86b8b8d4d4
miyaiaya <westtaila42240@gmail.com>
2018-12-01 19:23:28 +0900

    create js file

--- app/assets/javascripts/user-show.js
+++ app/assets/javascripts/user-show.js
@@ -9,36 +9,4 @@ $(document).on('turbolinks:load', function(){
     $(".user-container").hide();
     $(".user-profile").show();
   });
-
-  function readURL(input){
-    if(input.files && input.files[0]){
-      var reader = new FileReader();
-
-      reader.onload = function(e){
-        $("#header_image_prev").attr('src', e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-  $("#user_header_image").change(function(){
-    $("#header_image_prev").removeClass("hidden").show();
-    $(".header_present_image").remove();
-    readURL(this);
-  });
-
-  function readURL(input){
-    if(input.files && input.files[0]){
-      var reader = new FileReader();
-
-      reader.onload = function(e){
-        $("#main_image_prev").attr('src', e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-  $("#user_main_image").change(function(){
-    $("#main_image_prev").removeClass("hidden").show();
-    $(".my-icon").remove();
-    readURL(this);
-  })
 });
