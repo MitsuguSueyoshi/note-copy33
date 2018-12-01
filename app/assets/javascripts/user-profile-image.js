08e50a08ef2c7e9b619d4511cc589e1b217e52e8
miyaiaya <westtaila42240@gmail.com>
2018-12-01 19:37:29 +0900

    Get together js file

--- app/assets/javascripts/user-profile-image.js
+++ app/assets/javascripts/user-profile-image.js
@@ -4,6 +4,22 @@ $(document).on('turbolinks:load', function(){
       var reader = new FileReader();
 
       reader.onload = function(e){
+        $("#main_image_prev").attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#user_main_image").change(function(){
+    $("#main_image_prev").removeClass("hidden").show();
+    $(".my-icon").remove();
+    readURL(this);
+  });
+
+    function readURL(input){
+    if(input.files && input.files[0]){
+      var reader = new FileReader();
+
+      reader.onload = function(e){
         $("#header_image_prev").attr('src', e.target.result);
       }
       reader.readAsDataURL(input.files[0]);
