4a9b505fecff7e0eda55fa1f2f4f23fdbfa39a51
miyaiaya <westtaila42240@gmail.com>
2018-12-01 16:14:13 +0900

    edit users/show.html.haml and controller redirect_to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to user_path
     else
       render :edit
     end
