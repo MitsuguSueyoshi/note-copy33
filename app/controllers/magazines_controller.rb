88a07b3a9b584d438649096bcbfb2e018bedd264
末吉貢 <mitsugu11281128@gmail.com>
2018-12-02 17:01:04 +0900

    def seven actions in magazines-con

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -1,6 +1,24 @@
 class MagazinesController < ApplicationController
+
   def index
   end
+
+  def new
+  end
+
   def show
   end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
