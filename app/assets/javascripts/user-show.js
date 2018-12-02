4ba298db4f3cde0e437378f469afae9f6bb28c54
miyaiaya <westtaila42240@gmail.com>
2018-12-02 18:34:50 +0900

    add jquery

--- app/assets/javascripts/user-show.js
+++ app/assets/javascripts/user-show.js
@@ -9,4 +9,12 @@ $(document).on('turbolinks:load', function(){
     $(".user-container").hide();
     $(".user-profile").show();
   });
+  $(".tab1_label").on("click", function(){
+    $("#product_magazine").hide();
+    $("#product_note").show();
+  });
+  $(".tab2_label").on("click", function(){
+    $("#product_note").hide();
+    $("#product_magazine").show();
+  });
 });
