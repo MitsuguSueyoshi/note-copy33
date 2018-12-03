c70539c0ff508111641ed30d85f8dcee6942df86
末吉貢 <mitsugu11281128@gmail.com>
2018-12-03 16:14:46 +0900

    add magazines relation to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :notes
   has_many :likes
   has_many :comments
+  has_many :magazines, dependent: :destroy
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
