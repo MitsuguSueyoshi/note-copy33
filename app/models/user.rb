702cc90a9936e2c64275f0295a7e48c2f95134a1
miyaiaya <westtaila42240@gmail.com>
2018-12-04 20:51:16 +0900

    Revert "delete method from user.rb and note.rb"
    This reverts commit ebba3af94e090e7f43b9db0664a0430acce3b74f.

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,7 @@ class User < ApplicationRecord
   mount_uploader :header_image, ImageUploader
   mount_uploader :main_image, ImageUploader
 
+  def posts
+    return Post.where(user_id: self.id)
+  end
 end
