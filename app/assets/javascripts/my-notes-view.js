5c5338c818803f254776a85faaaa6b68e1636c44
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-12-05 23:45:02 +0900

    Add delete balloon

--- app/assets/javascripts/my-notes-view.js
+++ app/assets/javascripts/my-notes-view.js
@@ -5,4 +5,11 @@ $(document).on('turbolinks:load', function(){
     $('.my-menu').toggle();
     return false
   });
+
+    $(function() {
+      $('.balloon__event').click(function() {
+        $(this).parent().siblings('.delete__balloon').toggle();
+        return false
+      });
+    });
 });
