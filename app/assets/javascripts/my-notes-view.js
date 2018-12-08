318059b72b76470100ccbdc1a9daa5a534a71a19
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-12-08 16:34:12 +0900

    Add js file for my notes

--- app/assets/javascripts/my-notes-view.js
+++ app/assets/javascripts/my-notes-view.js
@@ -1,11 +1,4 @@
 $(document).on('turbolinks:load', function(){
-  $("#my__notes__view").on("click", function(){
-    $(".home__container").hide();
-    $(".my-notes__container").show();
-    $('.my-menu').toggle();
-    return false
-  });
-
     $(function() {
       $('.balloon__event').click(function() {
         $(this).parent().siblings('.delete__balloon').toggle();
