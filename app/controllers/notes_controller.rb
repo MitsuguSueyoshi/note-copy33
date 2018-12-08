7bdce253d21f585eb996fdc9ae9619af0a97b826
末吉貢 <mitsugu11281128@gmail.com>
2018-12-08 16:35:13 +0900

    add @like @likes to note-con index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,6 +3,8 @@ class NotesController < ApplicationController
 
   def index
     @notes = Note.all.includes(:user).order("created_at DESC")
+    @like = Like.find_by(user_id: current_user.id)
+    @likes = Like.all.includes(:user)
   end
 
   def show
