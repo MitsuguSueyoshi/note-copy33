c6f5092bee471d7c8d5b0f1ada5e93303d6e270d
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-12-08 20:43:28 +0900

    Delete unnecessary description

--- app/assets/javascripts/my-notes-view.js
+++ app/assets/javascripts/my-notes-view.js
@@ -1,29 +1,20 @@
 $(document).on('turbolinks:load', function(){
-    $(function() {
-      $('.balloon__event').click(function() {
-        $(this).parent().siblings('.delete__balloon').toggle();
-        return false
-      });
-    });
-    $(function() {
-      $('input:checkbox').change(function() {
-          var cnt = $('.selection__box input:checkbox:checked').length;
-          $('span.checked__counts').text(cnt);
-      });
-    });
-    $(function() {
-      $('.times').on('click', function() {
-        $('.selection__box input:checkbox:checked').prop({'checked':false});
-      });
-    });
-
-    $(function() {
-      $('.selection__box input:checkbox').change(function() {
-        if ( $('.selection__box input:checkbox:checked').length > 0 ) {
-          $('.my-notes__foot').show();
-        } else {
-          $('.my-notes__foot').hide();
-        }
-      });
-    });
+  $('.balloon__event').click(function() {
+    $(this).parent().siblings('.delete__balloon').toggle();
+    return false
+  });
+  $('input:checkbox').change(function() {
+      var cnt = $('.selection__box input:checkbox:checked').length;
+      $('span.checked__counts').text(cnt);
+  });
+  $('.times').on('click', function() {
+      $('.selection__box input:checkbox:checked').prop({'checked':false});
+  });
+  $('.selection__box input:checkbox').change(function() {
+      if ( $('.selection__box input:checkbox:checked').length > 0 ) {
+        $('.my-notes__foot').show();
+      } else {
+        $('.my-notes__foot').hide();
+      }
+  });
 });
