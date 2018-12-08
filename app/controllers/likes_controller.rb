3b4e15119eb5b12be67e47fa7eb12e9a75935282
末吉貢 <mitsugu11281128@gmail.com>
2018-12-08 16:33:28 +0900

    create & complete like-con

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,16 @@
 class LikesController < ApplicationController
+  def create
+    @like = Like.create(user_id: current_user.id, note_id: params[:note_id])
+    @likes = Like.where(note_id: params[:note_id])
+    @notes = Note.all
+    redirect_to notes_path
+  end
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, note_id: params[:note_id])
+    @like.destroy
+    @likes = Like.where(note_id: params[:note_id])
+    @notes = Note.all
+    redirect_to notes_path
+  end
 end
