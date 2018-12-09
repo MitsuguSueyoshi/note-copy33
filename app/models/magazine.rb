16513bfabaa26ff8dbe62b167b2cf2624a30d18d
末吉貢 <mitsugu11281128@gmail.com>
2018-12-09 17:53:55 +0900

    add method about article

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,5 +1,20 @@
 class Magazine < ApplicationRecord
   belongs_to :user
+  has_many :articles
   has_many :notes, through: :articles
   mount_uploader :header_image, ImageUploader
+
+
+  def adding(note)
+    self.articles.find_or_create_by(note_id: note.id)
+  end
+
+  def unadding(note)
+    article = self.articles.find_by(note_id: note.id)
+    article.destroy if article
+  end
+
+  def adding?(note)
+    self.notes.include?(note)
+  end
 end
