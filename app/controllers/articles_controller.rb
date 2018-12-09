4e531669032ba57df9f72025d6bb7b4f79b277e9
末吉貢 <mitsugu11281128@gmail.com>
2018-12-09 17:55:34 +0900

    complete article-con

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,38 @@
 class ArticlesController < ApplicationController
+  before_action :set_note
+
   def create
+    note = Note.find(params[:article][:note_id])
+    magazine = Magazine.find(params[:article][:magazine_id])
+    adding = magazine.adding(note)
+    if adding.save
+      respond_to do |format|
+        format.html
+        format.json
+      end
+    else
+      flash[:alert] = "失敗しました"
+    end
   end
 
   def destroy
+    note = Note.find(params[:article][:note_id])
+    magazine = Magazine.find(params[:article][:magazine_id])
+    adding = magazine.unadding(note)
+    if adding.destroy
+      respond_to do |format|
+        format.html
+        format.json
+      end
+    else
+      flash[:alert] = "失敗しました"
+    end
   end
+
+  private
+
+  def set_note
+    note = Note.find(params[:article][:note_id])
+  end
+
 end
