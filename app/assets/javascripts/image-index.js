9ac3b6aefa940b49af4831b29d350546179d990d
miyaiaya <westtaila42240@gmail.com>
2018-12-09 13:50:57 +0900

    delete console.log

--- app/assets/javascripts/image-index.js
+++ app/assets/javascripts/image-index.js
@@ -1,12 +1,10 @@
 $(document).on('turbolinks:load', function(){
   $(".layout-image").on("click" , function(){
-    var imageSrc = $(this).attr('src')
-    console.log(imageSrc);
+    var imageSrc = $(this).attr('src');
     $("#expantion-image").attr('src', imageSrc);
     $(".image-modal").fadeIn();
   });
   $(".close-button").on("click", function(){
   $(".image-modal").fadeOut();
   })
-
 });
