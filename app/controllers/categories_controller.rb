5229c53625fd3af5b7c8b55abf93e4dce90cbf09
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-12-13 22:47:35 +0900

    Change variable for switch

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
-    @notes = Note.where(category: params[:category])
+    @hot_notes = Note.where(category: params[:category])
+    @new_notes = Note.where(category: params[:category]).order("created_at DESC")
   end
 end
