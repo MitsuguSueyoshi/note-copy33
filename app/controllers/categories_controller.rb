082b0967423d6e4530bf249ca4d1f66c8860939b
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-12-15 15:18:29 +0900

    Change variable for @hot_notes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
 
   def index
-    @hot_notes = Note.where(category: params[:category])
+    notes = Note.find(Like.group(:note_id).order('count(note_id) desc').pluck(:note_id))
+    @hot_notes = notes.select{ |note| note.category_before_type_cast == params[:category].to_i }
     @new_notes = Note.where(category: params[:category]).order("created_at DESC")
   end
 end
