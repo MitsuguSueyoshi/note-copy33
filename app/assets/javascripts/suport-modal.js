fe141ecd215a3018d0072d08ad5e78428b25e127
miyaiaya <westtaila42240@gmail.com>
2018-12-16 17:30:18 +0900

    add fixScroll and scrollRelease function

--- app/assets/javascripts/suport-modal.js
+++ app/assets/javascripts/suport-modal.js
@@ -1,13 +1,39 @@
 $(document).on('turbolinks:load', function(){
+  var current_scrollY;
+  function fixScroll(){
+    current_scrollY= $(window).scrollTop();
+    $("body").css({
+      position: "fixed",
+      width: "100%",
+      top: -1 * current_scrollY
+    });
+  };
+
+  function scrollRelease(){
+    $("body").css({
+      position: "",
+      width: "",
+      top: ""
+    });
+    $(window).scrollTop(current_scrollY);
+  };
+
+
   $(".note__support__content__button").on("click", function(){
+    fixScroll();
     $(".suport").fadeIn();
   });
+
   $(".suport-cancel").on("click", function(){
+    scrollRelease();
     $(".suport").fadeOut();
   });
+
   $(".suport-submit").on("click", function(){
+    scrollRelease();
     $(".suport").fadeOut();
   });
+
   $(".suport-modal__price__block__select").on("click", function(){
     $(".suport-modal__price__block__select__image.selected").removeClass("selected");
     $(this).children(".suport-modal__price__block__select__image").addClass("selected");
