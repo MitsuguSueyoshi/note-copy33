6b225043db55dfe139c56bfdebad3de7a454e900
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-12-16 20:07:26 +0900

    Set variable for index of pickup controller

--- app/controllers/pickups_controller.rb
+++ app/controllers/pickups_controller.rb
@@ -1,2 +1,8 @@
 class PickupsController < ApplicationController
+
+  def index
+    @hot_notes = Note.includes(:user).find(Like.group(:note_id).order('count(note_id) desc').pluck(:note_id))
+    @new_notes = Note.all.includes(:user).order("created_at DESC")
+    @old_notes = Note.all.includes(:user).order("created_at ASC")
+  end
 end
