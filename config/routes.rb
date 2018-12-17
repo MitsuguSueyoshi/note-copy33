3d4155965cd17e3889f0d5ad058e71f1e8246834
miyaiaya <westtaila42240@gmail.com>
2018-12-17 23:37:50 +0900

    add resources :wanted, only: [:index]

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   resources :helps, only:[:index]
   resources :articles , only: [:create, :destroy]
   resources :categories, only: [:index]
+  resources :wanted, only: [:index]
 end
