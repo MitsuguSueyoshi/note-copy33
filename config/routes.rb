28426de355b51d8d9e1416c4d6709ab4bb6d35db
miyaiaya <westtaila42240@gmail.com>
2018-12-21 21:14:15 +0900

    add resources: header magazine to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,5 @@ Rails.application.routes.draw do
   resources :categories, only: [:index]
   resources :wanted, only: [:index]
   resources :pickups, only: [:index]
+  resources :headermagazines, only: [:index]
 end
