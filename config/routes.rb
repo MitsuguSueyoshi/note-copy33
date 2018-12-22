d566834c6a91b6e84f4a43c0f34ab309a07d868b
miyaiaya <westtaila42240@gmail.com>
2018-12-22 17:55:26 +0900

    add magazine_all action

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,9 @@ Rails.application.routes.draw do
   resources :categories, only: [:index]
   resources :wanted, only: [:index]
   resources :pickups, only: [:index]
-  resources :headermagazines, only: [:index]
+  resources :headermagazines, only: [:index] do
+    collection do
+      get 'magagine_all'
+    end
+  end
 end
