2fead26cbbcce44234de9362e04caed66ca47338
miyaiaya <westtaila42240@gmail.com>
2018-12-22 18:26:38 +0900

    create magazine_all action

--- app/controllers/headermagazines_controller.rb
+++ app/controllers/headermagazines_controller.rb
@@ -4,4 +4,8 @@ class HeadermagazinesController < ApplicationController
     @free_magazines = Magazine.all.includes(:user).order("created_at DESC").limit(6)
     @pickup_magazine = Magazine.last
   end
+
+  def magazine_all
+    @all_magazines = Magazine.all.includes(:user).order("created_at DESC")
+  end
 end
