e8e41f2629dafcead2ad7b31938625b184a85811
miyaiaya <westtaila42240@gmail.com>
2018-12-22 21:15:33 +0900

    use get_magazine method

--- app/controllers/headermagazines_controller.rb
+++ app/controllers/headermagazines_controller.rb
@@ -1,11 +1,15 @@
 class HeadermagazinesController < ApplicationController
 
   def index
-    @free_magazines = Magazine.all.includes(:user).order("created_at DESC").limit(6)
+    @free_magazines = Magazine.get_magazine.limit(6)
     @pickup_magazine = Magazine.last
   end
 
   def magazine_all
-    @all_magazines = Magazine.all.includes(:user).order("created_at DESC")
+    @all_magazines = Magazine.get_magazine
   end
+
+
+
+
 end
