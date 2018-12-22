bbbf4c7a60286c455381885e82f36d86ed7dda89
miyaiaya <westtaila42240@gmail.com>
2018-12-22 21:36:33 +0900

    change position of scope

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -4,6 +4,7 @@ class Magazine < ApplicationRecord
   has_many :notes, through: :articles
   mount_uploader :header_image, ImageUploader
 
+  scope :get_magazine, -> { all.includes(:user).order("created_at DESC")}
 
   def adding(note)
     self.articles.find_or_create_by(note_id: note.id)
@@ -17,7 +18,5 @@ class Magazine < ApplicationRecord
   def adding?(note)
     self.notes.include?(note)
   end
-
-  scope :get_magazine, -> { all.includes(:user).order("created_at DESC")}
 
 end
