a371bfab1539bd143c08f1ab841b1c1116d0bb7b
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-12-23 18:49:01 +0900

    Add scope

--- app/controllers/attentions_controller.rb
+++ app/controllers/attentions_controller.rb
@@ -1,14 +1,14 @@
 class AttentionsController < ApplicationController
 
   def index
-    @manga_notes = Note.where(category: 0).includes(:user).order("created_at DESC").limit(9)
-    @column_notes = Note.where(category: 1).includes(:user).order("created_at DESC").limit(9)
-    @novel_notes = Note.where(category: 2).includes(:user).order("created_at DESC").limit(9)
-    @photo_notes = Note.where(category: 3).includes(:user).order("created_at DESC").limit(9)
-    @sound_notes = Note.where(category: 4).includes(:user).order("created_at DESC").limit(9)
-    @business_notes = Note.where(category: 5).includes(:user).order("created_at DESC").limit(9)
-    @lifestyle_notes = Note.where(category: 6).includes(:user).order("created_at DESC").limit(9)
-    @technology_notes = Note.where(category: 7).includes(:user).order("created_at DESC").limit(9)
-    @entertainment_notes = Note.where(category: 8).includes(:user).order("created_at DESC").limit(9)
+    @manga_notes = Note.search_with_category(0).recent
+    @column_notes = Note.search_with_category(1).recent
+    @novel_notes = Note.search_with_category(2).recent
+    @photo_notes = Note.search_with_category(3).recent
+    @sound_notes = Note.search_with_category(4).recent
+    @business_notes = Note.search_with_category(5).recent
+    @lifestyle_notes = Note.search_with_category(6).recent
+    @technology_notes = Note.search_with_category(7).recent
+    @entertainment_notes = Note.search_with_category(8).recent
   end
 end
