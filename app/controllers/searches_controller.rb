311fdad1fca08a93c13c3d3a2ab6475397aaa77d
miyaiaya <westtaila42240@gmail.com>
2018-12-23 19:50:50 +0900

    edit searches_controller index action

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,11 @@
 class SearchesController < ApplicationController
 
  def index
-
+  @search_notes = Note.ransack(params[:q])
+  @result_notes = @search_notes.result
+  @likes = Like.all.includes(:user)
+  if user_signed_in?
+    @follow_users = User.find(current_user.id).followings
+  end
  end
 end
