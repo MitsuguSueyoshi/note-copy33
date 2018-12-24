63ed676e5bcf73d8765c4b80d7ba143fd834c587
miyaiaya <westtaila42240@gmail.com>
2018-12-24 21:46:47 +0900

    add def search_magazine to controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,16 @@
 class SearchesController < ApplicationController
 
- def index
-  @search_notes = Note.ransack(params[:q])
-  @result_notes = @search_notes.result
-  @likes = Like.all.includes(:user)
-  if user_signed_in?
-    @follow_users = User.find(current_user.id).followings
+  def index
+    @search_notes = Note.ransack(params[:q])
+    @result_notes = @search_notes.result
+    @likes = Like.all.includes(:user)
+    if user_signed_in?
+      @follow_users = User.find(current_user.id).followings
+    end
   end
- end
+
+  def search_magazine
+    @search_magazines = Magazine.ransack(params[:q])
+    @result_magazines = @search_magazines.result
+  end
 end
