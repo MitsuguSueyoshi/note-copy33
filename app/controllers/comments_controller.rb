2f189069f8bab7d89d15429213b911d47a7edfe9
末吉貢 <mitsugu11281128@gmail.com>
2018-12-26 20:23:05 +0900

    create def create details in comments_con

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :set_note
+
   def create
+    @comment = @note.comments.new(comment_params)
+    if @comment.save
+      respond_to do |format|
+        format.html{ redirect_to note_comments_path(@comment), notice: 'メッセージが送信されました' }
+        format.json
+      end
+    else
+      @comments = @note.comments.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください'
+      redirect_to note_path(@note)
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(user_id: current_user.id)
+  end
+
+  def set_note
+    @note = Note.find(params[:note_id])
   end
 end
