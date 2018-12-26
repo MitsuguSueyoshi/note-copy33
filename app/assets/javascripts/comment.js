1355ebfbf2470014cb90a6968de6990c599e4124
末吉貢 <mitsugu11281128@gmail.com>
2018-12-26 21:59:19 +0900

    complete process of comment-create

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,5 +1,34 @@
 $(document).on('turbolinks:load', function(){
   $(function(){
+
+    function buildHTML(comment){
+      var html = `
+      <div class="comments__comment">
+        <div class="comments__comment__profile">
+          <div class="comments__comment__profile__icon">
+            <img src="${comment.user_image}">
+          </div>
+          <div class="comments__comment__profile__name">
+            ${comment.user_name}
+          </div>
+          <div class="comments__comment__profile__date">
+            ${comment.time}
+          </div>
+        </div>
+        <div class="comments__comment__option">
+          <i class="fa fa-ellipsis-h"></i>
+        </div>
+        <div class="comments__comment__likes">
+          <i class="far fa-heart"></i>
+        </div>
+        <div class="comments__comment__body">
+          ${comment.body}
+        </div>
+      </div>`
+
+      return html;
+    }
+
     $('.comments__form__content__text').on("click", function(e){
       $('.comments__form__content__submit').fadeIn();
     })
@@ -7,6 +36,34 @@ $(document).on('turbolinks:load', function(){
     $('.footer').on("click", function(e){
       $('.comments__form__content__submit').fadeOut();
     })
+
+
+    $(".comments__form__content").on("submit", function(e){
+      e.preventDefault();
+      var content = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: content,
+        dataType: "json",
+        processData: false,
+        contentType: false
+      })
+      .done(function(comment){
+        var html = buildHTML(comment);
+        $(".comments").append(html);
+        $('.comments').animate({scrollTop: $(".comments")[0].scrollHeight},'slow');
+        $(".comments__form__content__text").val("");
+        $('.comments__form__content__submit').prop('disabled', false);
+      })
+      .fail(function(XMLHttpRequest, textStatus, errorThrown){
+        alert('メッセージの送信に失敗しました');
+        console.log(XMLHttpRequest.status);
+        console.log(textStatus);
+        console.log(errorThrown);
+      })
+    });
 
   });
 });
