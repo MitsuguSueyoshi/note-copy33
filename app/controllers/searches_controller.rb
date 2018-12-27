0431da70daf8d6754208d7dab43113184a4104d0
miyaiaya <westtaila42240@gmail.com>
2018-12-27 20:42:01 +0900

    add new_notes, spike_notes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,10 @@ class SearchesController < ApplicationController
 
   def index
     @search_notes = Note.ransack(params[:q])
-    @result_notes = @search_notes.result
+    @result_notes = @search_notes.result.includes(:user)
+    @new_notes = @search_notes.result.order("created_at DESC").includes(:user)
+    @spike_notes = @search_notes.result.order("created_at ASC").includes(:user)
+
     @likes = Like.all.includes(:user)
     if user_signed_in?
       @follow_users = User.find(current_user.id).followings
