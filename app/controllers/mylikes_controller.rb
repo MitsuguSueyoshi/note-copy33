2048c469fb53fef786f4e3a90f6808bf3e9e54e3
末吉貢 <mitsugu11281128@gmail.com>
2019-01-01 15:20:12 +0900

    add @liikes (order created_at DESC)

--- app/controllers/mylikes_controller.rb
+++ app/controllers/mylikes_controller.rb
@@ -1,5 +1,5 @@
 class MylikesController < ApplicationController
   def index
-    @likes = current_user.likes if user_signed_in?
+    @likes = current_user.likes.order("created_at DESC") if user_signed_in?
   end
 end
