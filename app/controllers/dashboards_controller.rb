c98780fca397a0d9d6ce053181f9a3db09cc5593
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2019-01-05 14:01:37 +0900

    Add like and comment count

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,12 @@
 class DashboardsController < ApplicationController
 
   def index
-
+    @notes = Note.where(user_id: current_user.id)
+    @comments_count = 0
+    @likes_count = 0
+    @notes.each do |note|
+      @comments_count += note.comments.count
+      @likes_count += note.likes.count
+    end
   end
 end
