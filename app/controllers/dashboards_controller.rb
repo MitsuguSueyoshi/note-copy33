d04caf191f01435b32c8a3b866b8e64092449499
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2019-01-05 14:37:03 +0900

    Change html description for view count

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,12 @@
 class DashboardsController < ApplicationController
 
   def index
-    @notes = Note.where(user_id: current_user.id)
+    @notes = Note.where(user_id: current_user.id).order("created_at DESC")
+    @views_count = 0
     @comments_count = 0
     @likes_count = 0
     @notes.each do |note|
+      @views_count += note.impressionist_count
       @comments_count += note.comments.count
       @likes_count += note.likes.count
     end
