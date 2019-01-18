8dfd76e4372b55a8ecb3558e42550da5666c4155
miyaiaya <westtaila42240@gmail.com>
2019-01-18 19:08:59 +0900

    Delete recaptcha

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,17 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  prepend_before_action :check_captcha, only: [:create]
-  prepend_before_action :customize_sign_up_params, only: [:create]
+  # prepend_before_action :check_captcha, only: [:create]
+  # prepend_before_action :customize_sign_up_params, only: [:create]
 
-  private
-    def customize_sign_up_params
-      devise_parameter_sanitizer.permit :sign_up, keys: [:nickname, :email, :password]
-    end
+  # private
+  #   def customize_sign_up_params
+  #     devise_parameter_sanitizer.permit :sign_up, keys: [:nickname, :email, :password]
+  #   end
 
-    def check_captcha
-      self.resource = resource_class.new sign_up_params
-      resource.validate
-      unless verify_recaptcha(model: resource)
-        respond_with_navigational(resource) {render :new}
-      end
-    end
+  #   def check_captcha
+  #     self.resource = resource_class.new sign_up_params
+  #     resource.validate
+  #     unless verify_recaptcha(model: resource)
+  #       respond_with_navigational(resource) {render :new}
+  #     end
+  #   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: 'users/registrations'}
+  devise_for :users
   root 'notes#index'
   resources :images, only: [:index]
   resources :users, only: [:show, :edit, :update] do
