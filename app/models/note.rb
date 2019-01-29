2e40dbf9997c715baff5c9d341f6d9b5cfddf003
末吉貢 <mitsugu11281128@gmail.com>
2019-01-29 22:24:47 +0900

    def self.create_all_ranks

--- app/models/note.rb
+++ app/models/note.rb
@@ -32,4 +32,7 @@ class Note < ApplicationRecord
     Note.where(user_id: user_id).order("created_at DESC")
   end
 
+  def self.create_all_ranks
+    Note.find(Like.group(:note_id).order('count(note_id) desc').limit(3).pluck(:note_id))
+  end
 end
