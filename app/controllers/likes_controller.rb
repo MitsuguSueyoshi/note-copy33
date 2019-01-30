cd5d95a93cba8c9a4d42699a0c4bfb0f6a34b355
末吉貢 <mitsugu11281128@gmail.com>
2019-01-30 11:46:02 +0900

    delete @notes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,6 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, note_id: params[:note_id])
     @likes = Like.where(note_id: params[:note_id])
-    @notes = Note.all
     redirect_back(fallback_location: root_path)
   end
 
@@ -10,7 +9,6 @@ class LikesController < ApplicationController
     @like = Like.find_by(user_id: current_user.id, note_id: params[:note_id])
     @like.destroy
     @likes = Like.where(note_id: params[:note_id])
-    @notes = Note.all
     redirect_back(fallback_location: root_path)
   end
 end
